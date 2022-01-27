e9dfe89066e4f908eff516c7852c1f6bf78f3bc0
Ivan Feliciano Avelino <ivan.feliciano@kueski.com>
2022-01-26 19:48:54 -0600

    [FEATURE-2] Change test values

--- test/module/a.py
+++ test/module/a.py
@@ -3,11 +3,11 @@ import pytest
 
 @pytest.fixture()
 def number():
-    return 4
+    return 9
 
 def func(x):
     return x + 1
 
 
 def test_answer(number):
-    assert func(number) == 5
+    assert func(number) == 10
