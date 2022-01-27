5dd45be44b6aade619d71d167292678ae32693d8
Ivan Feliciano Avelino <ivan.feliciano@kueski.com>
2022-01-27 09:03:36 -0600

    [FEATURE-2] Change test

--- test/module/a.py
+++ test/module/a.py
@@ -1,13 +1,6 @@
-import pytest
-
-
-@pytest.fixture()
-def number():
-    return 9
-
 def func(x):
     return x + 1
 
 
-def test_answer(number):
-    assert func(number) == 10
+def test_answer():
+    assert func(9) == 10
